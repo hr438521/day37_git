3f3ca59a8aee0e60e4bb114e3d9a5dff7d13eacc
hr438521 <51064845+hr438521@users.noreply.github.com>
2019-05-28 18:14:36 +0800

    qewq

--- src/com/qf/test/App.java
+++ src/com/qf/test/App.java
@@ -2,7 +2,9 @@ package com.qf.test;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("ww");
+        System.out.println("heihei");
         System.out.println("哈哈");
-        //System.out.println("heihei");
+        System.out.println("heihei");
     }
 }
